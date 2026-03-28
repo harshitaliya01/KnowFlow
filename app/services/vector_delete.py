bf75984e9078bab10a7e7c5d045f90f84b4170b1
Harsh <harshitaliya2004@gmail.com>
2026-03-28 16:55:46 +0530

    delete vector ok

--- app/services/vector_delete.py
+++ app/services/vector_delete.py
@@ -9,33 +9,14 @@ async def delete_vectors(doc_id: int):
         # api_key=os.getenv("QDRANT_API_KEY"),
     )
 
-    search_result = await client.scroll(
-        collection_name=collection,
-        scroll_filter=Filter(
-            must=[
-                FieldCondition(
-                    key="document_id",
-                    match=MatchValue(value=str(doc_id))
-                )
-            ]
-        ),
-        limit=1,
-        with_payload=False, 
-        with_vectors=False
-    )
-    points = search_result[0]
-
-    if points:
-        await client.delete(
+    await client.delete(
             collection_name=collection,
             points_selector=Filter(
                 must=[
                     FieldCondition(
-                        key="document_id",
+                        key="metadata.document_id",
                         match=MatchValue(value=str(doc_id))
                     )
                 ]
             )
-        )
-    else:
-        pass
+    )
